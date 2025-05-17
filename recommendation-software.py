43cb7fb499def15d1c47a18f466dba5af3e76ac3
M <145228723+marco-mr@users.noreply.github.com>
2025-05-17 16:41:30 +0200

    Added comments for readability

--- recommendation-software.py
+++ recommendation-software.py
@@ -5,7 +5,8 @@ class Recommendation:
         self.csv = pd.read_csv("steam_dataset.csv")
         self.genres = self.csv["Genres"].unique()
         self.genres_lower = {genre.lower(): genre for genre in self.genres}
-
+        
+    # Greet user
     def start(self):
         print("------------------------------------------")
         print("| Welcome to the Steam game recommender! |")
@@ -13,6 +14,7 @@ class Recommendation:
         print("------------------------------------------")
         self.get_user_input()
 
+    # Get genre from user and convert to lowercase
     def get_user_input(self):
         print("\n-----Choose a genre!-----\n")
         for genre in sorted(self.genres):
@@ -24,10 +26,12 @@ class Recommendation:
                 print("Not a valid genre!")
                 continue
             break
+        # Convert lowercase back to original format and select the games with the corresponding genre from the csv
         selected_genre = self.genres_lower[genre_choice]
         self.games_with_genre = self.csv[self.csv["Genres"] == selected_genre]
         self.get_user_action()
 
+    # Get input from user how they want the data displayed
     def get_user_action(self):
         actions = {
             "Top 10": lambda: self.display_top_10(),
@@ -45,6 +49,7 @@ class Recommendation:
             break
         actions[action_choice]()
 
+    # Print to Terminal in different format for better readability
     def display(self, games_to_display):
         print("\n")
         for _, row in games_to_display.iterrows():
@@ -52,10 +57,12 @@ class Recommendation:
                 print(f"{col}: {value}")
             print('-' * 50)
         self.reprompt()
-
+        
+    # Choose the 10 best rated games in the genre
     def display_top_10(self):
         self.display(self.games_with_genre.head(10))
-    
+
+    # Choose to a range of games
     def display_range(self):
         print(f"There are {len(self.games_with_genre)} games available")
         while True:
@@ -68,14 +75,17 @@ class Recommendation:
             except ValueError:
                 print(f"Invalid range. Please enter a valid range (1 to {len(self.games_with_genre)}).")
         self.display(self.games_with_genre.iloc[start:end])
-        
-    def display_random(self):
-        self.display(self.games_with_genre.sample(5))
 
+    # Choose to get 5 random games 
+    def display_random(self):
+        self.display(self.games_with_genre.sample(5)) # change "5" to how many random games you would want displayed
+
+    # Choose to display every listed in the genre
     def display_all(self):
         with pd.option_context("display.max_rows", None):
             self.display(self.games_with_genre)
 
+    # Reprompt user if they want to choose a different genre, display style or exit the program
     def reprompt(self):
         actions = {
             "1": lambda: self.get_user_input(),
@@ -91,6 +101,7 @@ class Recommendation:
             print("Not a valid input!\n")
         
 
+# Run the program
 if __name__ == "__main__":
     recommendation_software = Recommendation()
     recommendation_software.start()
